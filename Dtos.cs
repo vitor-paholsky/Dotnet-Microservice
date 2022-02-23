d982d0e686a986bf3c27698c2cb5d259f9f641b2
Vitor Paholsky <vitor.duarte@fulltechpro.com.br>
2022-02-23 15:49:04 -0300

    controllers

---
+++ Dtos.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Play.Catalog.Service.Dtos
+{
+    public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreateDate);
+    public record CreateItemDto(string Name, string Description, decimal Price);
+    public record UpdateItemDto(string Name, string Description, decimal Price);
+
+}
