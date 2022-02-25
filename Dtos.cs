2b312902c7df3d4af73b297f9d8556ceb526d3e2
Vitor Paholsky <vitor.duarte@fulltechpro.com.br>
2022-02-25 17:04:43 -0300

    async

--- Dtos.cs
+++ Dtos.cs
@@ -1,9 +1,25 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Play.Catalog.Service.Dtos
 {
-    public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreateDate);
-    public record CreateItemDto(string Name, string Description, decimal Price);
-    public record UpdateItemDto(string Name, string Description, decimal Price);
+    public record ItemDto(
+        Guid Id, 
+        string Name, 
+        string Description, 
+        decimal Price, 
+        DateTimeOffset CreateDate);
+    public record CreateItemDto(
+        [Required] 
+        string Name, 
+        string Description,
+        [Range(0, 1000)] 
+        decimal Price);
+    public record UpdateItemDto(
+        [Required] 
+        string Name, 
+        string Description,
+        [Range(0, 1000)]  
+        decimal Price);
 
 }
